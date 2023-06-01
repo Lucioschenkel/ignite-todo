b897ae7368f97a65843158147ee4986953972334
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2023-06-01 17:57:37 -0300

    fix: linting error

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,5 +5,6 @@ import { theme } from '../styles/theme';
 type ThemeType = typeof theme;
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends ThemeType {}
 }
