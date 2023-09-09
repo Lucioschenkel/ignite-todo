0d4fe606552139098f5c18dd7c9cf554090fbfda
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2023-09-09 16:28:02 -0300

    chore: refactors home page component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,24 @@
 import { useContext } from 'react';
 import { PlusCircle } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { TextInput } from '../../components/TextInput';
 import {
-  EmptyTaskList,
   HeaderText,
   HomeContainer,
   SectionContainer,
   TaskForm,
-  TaskList,
   TaskListContainer,
   TaskListHeader,
 } from './styles';
 import { Badge } from '../../components/Badge';
 import { TodoContext } from '../../contexts/TodoContext';
 
-import clipboardIcon from '../../assets/clipboard.png';
-import { Typography } from '../../components/Typography';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Task } from '../../components/Task';
+import { TaskList } from '../../components/TaskList';
 
 const addTaskSchema = z.object({
   title: z.string().min(3),
@@ -72,23 +68,7 @@ export function Home() {
               </Badge>
             </HeaderText>
           </TaskListHeader>
-          {tasks.length > 0 ? (
-            <TaskList data-testid="tasks">
-              {tasks.map((task) => (
-                <Task key={task.id} task={task} />
-              ))}
-            </TaskList>
-          ) : (
-            <EmptyTaskList>
-              <img src={clipboardIcon} alt="" />
-              <Typography weight="bold">
-                Você ainda não tem tarefas cadastradas
-              </Typography>
-              <Typography>
-                Crie tarefas e organize seus itens a fazer
-              </Typography>
-            </EmptyTaskList>
-          )}
+          <TaskList />
         </TaskListContainer>
       </SectionContainer>
     </HomeContainer>
